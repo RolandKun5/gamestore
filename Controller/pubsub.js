96a4e51847c30ce40fe8675b33c714b722733754
RolandKun5 <79998158+RolandKun5@users.noreply.github.com>
2021-08-04 23:12:54 +0200

    Add files via upload

---
+++ Controller/pubsub.js
@@ -0,0 +1,29 @@
+(function(){
+    class PubSub{
+        constructor(){
+            this.topics = [];
+        }
+        hasHandler(topicName){
+            return this.topics.hasOwnProperty(topicName);
+        }
+        subscribe(topicName,handler){
+            if(!this.hasHandler(topicName)){
+                this.topics[topicName] = [];
+            }
+            this.topics[topicName].push(handler);
+        }
+        publish(topicName,data){
+            if(!this.hasHandler(topicName)){
+                console.log(`Hiba, a(z) ${topicName} nincs feliratkozva!`);
+            }
+            else{
+                this.topics[topicName].forEach((handler)=>{
+                    handler(data);
+                })
+            }
+        }
+    }
+
+    window.PubSub = new PubSub();
+
+})();
